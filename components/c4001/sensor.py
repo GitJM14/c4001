26021a986eda725358a4b1d6f3352ec8c172c139
GitJM14 <128645379+GitJM14@users.noreply.github.com>
2025-03-26 09:47:44 +0000

    Update sensor.py

--- components/c4001/sensor.py
+++ components/c4001/sensor.py
@@ -22,7 +22,7 @@ CONFIG_SCHEMA = (
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_sensor(var, config)  # ✅ Correct function
+    await sensor.register_sensor(var, config)  # ✅ Correct
     await cg.register_component(var, config)
     await uart.register_uart_device(var, config)
 
